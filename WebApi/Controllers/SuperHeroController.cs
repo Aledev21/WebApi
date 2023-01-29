f2f1250a6b6bfcaea7c04e5035a5a7db3f45f949
Alessandro Assis <aledev21@gmail.com>
2023-01-29 12:08:45 -0300

    Add: Create Heros Controller

---
+++ WebApi/Controllers/SuperHeroController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult> Get()
+        {
+            var hereos = new List<SuperHero>
+            {
+                new SuperHero
+                {
+                    Id = 1,
+                    Name = "Spider Man",
+                    FirstName = "Petter",
+                    LastName = "Parker",
+                    Place = "New York City"
+                }
+            };
+
+            return Ok(hereos);
+        }
+    }
+}
+        
